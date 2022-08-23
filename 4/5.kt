8fd5c4d546591ff5519adf84d499033df2c7fb1c
Ned <7358345+nedpals@users.noreply.github.com>
2022-08-23 09:57:58 +0800

    add 4th section

---
+++ 4/5.kt
@@ -0,0 +1,22 @@
+fun main() {
+
+  var ceil = Math.ceil(3.5)
+  println(ceil)
+  // Add your notes below
+  // ceil rounds up to its nearest integer. ceil is 4
+  
+  var sqrt = Math.sqrt(25.0) 
+  println(sqrt)
+  // Add your notes below
+  // sqrt returns the square root of the double. sqrt is 5
+
+  var abs = Math.abs(-20) 
+  println(abs)
+  // Add your notes below 
+  // abs returns the absolute number of the given number. abs is 20
+
+  var floor = Math.floor(6.9)
+  println(floor)
+  // Add your notes below 
+  // floor rounds down to its nearest integer. floor is 6
+}
