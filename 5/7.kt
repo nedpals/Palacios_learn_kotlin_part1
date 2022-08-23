5c422c418162dfea3f17ddc6b0e2a86d7e9db5aa
Ned <7358345+nedpals@users.noreply.github.com>
2022-08-23 10:22:18 +0800

    add 5th section

---
+++ 5/7.kt
@@ -0,0 +1,13 @@
+fun main() {
+  var expressionValue: Boolean
+  
+  if (!true && (false && true || !false)) {
+    println("Hello")
+  } else {
+    println("Goodbye")
+  }
+
+  // Write your code below
+  expressionValue = false
+  println(expressionValue)
+}
