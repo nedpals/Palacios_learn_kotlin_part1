6f126db329bbd73ad7c5bfa46d33ea5374f33113
Ned <7358345+nedpals@users.noreply.github.com>
2022-08-23 09:17:45 +0800

    add 1 code

---
+++ 1/4.kt
@@ -0,0 +1,9 @@
+// Code prints "Howdy, partner!" to the terminal.
+fun main() {
+  println("Howdy, partner!") 
+}
+
+/*
+Programming with Kotlin is Fun!
+
+*/
---
+++ 1/6.kt
@@ -0,0 +1,6 @@
+// Add your code below
+fun main() {
+  println(2022 - 2001)
+  println("Choose wellness, choose nestle")
+  // Prints the age and the quote
+}
